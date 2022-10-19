660b9698e7ef7a6c45b00396edf633759e7e7664
Hua Liu <hualiu@huas-air.myfiosgateway.com>
2022-10-19 12:16:39 -0400

    boilerplate working. pre-entry change

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: ['./client/app.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
