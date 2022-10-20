e704aa1240c264f3b4bdff84328795589d540788
Hua Liu <hualiu@huas-air.myfiosgateway.com>
2022-10-20 17:14:45 -0400

    backend auth done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,6 +10,6 @@ const { protect } = require('../middleWare/authMiddleware');
 //registration / login / get data
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
